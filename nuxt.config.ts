f2a0bd5b78b603a70692fb5a348905d226fd4da6
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-06-29 20:21:22 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import { fileURLToPath } from "url";
+
+export default defineNuxtConfig({
+  srcDir: "./src",
+
+  alias: {
+    "@img": fileURLToPath(new URL("./shared/images/", import.meta.url)),
+  },
+  
+  css: [
+    "@/app/scss/main.scss"
+  ],
+
+  app: {
+    head: {
+      title: "Golden Honey",
+    },
+  },
+
+  components: {
+    global: true,
+    dirs: [
+      "./components", 
+      "./shared/icons",
+      "./shared/components",
+    ],
+  },
+});
---
+++ src/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <h1 class="h1-text">hello world</h1>
+</template>
+
