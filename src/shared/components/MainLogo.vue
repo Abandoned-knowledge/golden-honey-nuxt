1d912aadc38026f3e43d230ee16b596bcfabd5b0
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-06-30 11:00:12 +0300

    add animation to MainLogo.vue

--- src/shared/components/MainLogo.vue
+++ src/shared/components/MainLogo.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+let isAnimate = ref(false);
+onMounted(() => setTimeout(() => (isAnimate.value = true), 500));
+</script>
 
 <template>
-  <div class="logo">
-    <LogoIcon />
+  <div class="logo" :class="{ animate: isAnimate }">
+    <LogoIcon class="logo__icon" />
     <span class="logo__text lora gradient-main">golden honey</span>
   </div>
 </template>
@@ -12,11 +15,40 @@
   display: flex;
   align-items: center;
   gap: 10px;
+  width: fit-content;
+
+  &__icon {
+    transition: all 0.4s ease;
+
+    scale: 0.5;
+    rotate: -180deg;
+    opacity: 0;
+    visibility: hidden;
+  }
 
   &__text {
     font-weight: bold;
     font-size: 18px;
     text-transform: capitalize;
+    overflow-x: hidden;
+    translate: -100%;
+    transition: all 0.4s ease 0.05s;
+    opacity: 0;
+    visibility: hidden;
+  }
+}
+
+.animate {
+  & .logo__icon {
+    scale: 1;
+    rotate: 0deg;
+    opacity: 100;
+    visibility: visible;
+  }
+  & .logo__text {
+    translate: 0%;
+    opacity: 100%;
+    visibility: visible;
   }
 }
 </style>
