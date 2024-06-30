c9087afdfbfd991e5aa390e9da40f6649863a655
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-06-30 09:41:26 +0300

    create default layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,21 @@ import { fileURLToPath } from "url";
 export default defineNuxtConfig({
   srcDir: "./src",
 
+  devtools: { enabled: false },
+
   alias: {
     "@img": fileURLToPath(new URL("./shared/images/", import.meta.url)),
   },
-
   css: ["@/app/css/normalize.css", "@/app/scss/main.scss"],
+
+  dir: {
+    layouts: "./app/layouts/",
+  },
 
   app: {
     head: {
       title: "Golden Honey",
+      link: [{ rel: "icon", type: "image/png", href: "favicon.ico" }],
     },
   },
   components: {
